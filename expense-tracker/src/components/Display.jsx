fbf27906b6f417bc50ded2519efe70b0ebf52367
naveed69 <naveed.desai.69@gmail.com>
2025-01-14 08:42:42 +0530

    frontend UI is ready

--- expense-tracker/src/components/Display.jsx
+++ expense-tracker/src/components/Display.jsx
@@ -1,19 +1,20 @@
 import "./Display.css";
 import Heropage from "./Heropage";
+import React from "react";
 
 const Display = () => {
   return (
     <>
       <h2 style={{ color: "#FFFFFF" }}>Expense Tracker</h2>
       <Heropage />
-      <div className="row">
-        <div className="col-md-8">
+      <div className="mainpage row mt-3">
+        <div className="col-md-7">
           <h2 style={{ color: "#FFFFFF" }}>Recent Transactions</h2>
-          <div></div>
+          <div className="transactions">No Transactions!</div>
         </div>
-        <div classNam="col-md-4">
+        <div className="col-md-4">
           <h2 style={{ color: "#FFFFFF" }}>Top Expenses</h2>
-          <div></div>
+          <div className="expenses"></div>
         </div>
       </div>
     </>
